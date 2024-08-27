98abe44db44754a83a37f15c73aa2e7f869ad182
Jay <jay.lligui@gmail.com>
2024-08-26 21:12:53 -0400

    Initial Commit

---
+++ MealsInformation/MealsInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealsInformationApp.swift
+//  MealsInformation
+//
+//  Created by Jay Lliguichushca on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MealsInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
